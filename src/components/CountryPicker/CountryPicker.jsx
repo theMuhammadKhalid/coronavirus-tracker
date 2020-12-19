6c41e87ca3740f679cad99e6e8d527115a5db182
Muhammad Khalid <themuhammadkhalid@gmail.com>
2020-12-19 22:55:49 +0500

    Partially CountryPicker Work Done

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import styles from "./CountryPicker.module.css";
 
-export default function CountryPicker(){
-    return(
-        <div className={styles.container}>
-            <h1>Country Picker</h1>
-        </div>
-    )
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+
+export default function CountryPicker() {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <div className={styles.container}>
+      <FormControl variant="outlined" fullWidth="true">
+        <InputLabel id="country-picker-label">Select</InputLabel>
+        <Select
+          labelId="country-picker-label"
+          id="select"
+          value={age}
+          onChange={handleChange}
+          label="Select"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 }
